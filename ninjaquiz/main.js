6aa13d3983b2597e1b9341b4e50c60bb17699328
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-10-08 10:14:06 +0200

    quiz chapter 3

--- ninjaquiz/main.js
+++ ninjaquiz/main.js
@@ -1,4 +1,22 @@
 alert("Welcome to Quiz Ninja!");
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}!`)
+
+const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"],
+    ["What is Spiderman's real name?", "Peter Parker"]
+];
+
+let score = 0;
+
+for (const [question, answer] of quiz) {
+    const response = prompt(question);
+    if (response === answer) {
+        alert("Correct!");
+        score++;
+    } else {
+       alert(`Wrong! The correct answer was ${answer}.`) 
+    }
+}
+
+alert(`Game over! You scored ${score} point${score !== 1 ? 's' : '' }.`);
