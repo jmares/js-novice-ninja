58bab78add6c4537750efb18a1373e845f20211d
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-10-13 12:22:42 +0200

    quiz chapter 5

--- ninjaquiz/main.js
+++ ninjaquiz/main.js
@@ -5,6 +5,19 @@ const quiz = [
     { name: "Spiderman", realName: "Peter Parker"}
 ];
 
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target, content, attributes) {
+        for (const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+}
+
 const game = {
     start(quiz) {
         this.questions = [...quiz];
@@ -17,19 +30,24 @@ const game = {
     },
     ask() {
         const question = ` What is ${this.question.name}'s real name?`;
+        view.render(view.question, question);
         const response = prompt(question);
         this.check(response);
     },
     check(response) {
         const answer = this.question.realName;
         if (response === answer) {
+            view.render(view.result, 'Correct!', {'class': 'correct'});
             alert('Correct!');
             this.score++;
+            view.render(view.score, this.score);
         } else {
+            view.render(view.result, `Wrong! The correct answer was ${answer}`, {'class': 'wrong'});
             alert(`Wrong! The correct answer was ${answer}`);
         }
     },
     gameOver() {
+        view.render(view.info, `Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
         alert(`Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 }
