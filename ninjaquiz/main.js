a09739cb55074be5e4a97d135d819bcc06623944
j0hm4r <67288966+j0hm4r@users.noreply.github.com>
2020-10-17 21:02:58 +0200

    quiz chapter 8

--- ninjaquiz/main.js
+++ ninjaquiz/main.js
@@ -11,6 +11,17 @@ const view = {
     result: document.getElementById('result'),
     info: document.getElementById('info'),
     start: document.getElementById('start'),
+    response: document.querySelector('#response'),
+    setup() {
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score, game.score);
+        this.render(this.result, '');
+        this.render(this.info, '');
+        this.resetForm();
+    },
     render(target, content, attributes) {
         for (const key in attributes) {
             target.setAttribute(key, attributes[key]);
@@ -22,45 +33,56 @@ const view = {
     },
     hide(element) {
         element.style.display = 'none';
+    },
+    resetForm() {
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+    tearDown() {
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
     }
-}
+};
 
 const game = {
     start(quiz) {
         this.questions = [...quiz];
         this.score = 0;
-        view.hide(view.start);
-        for (const question of this.questions) {
-            this.question = question;
-            this.ask();
+        view.setup();
+        this.ask();
+    },
+    ask(name) {
+        if (this.questions.length > 0) {
+            this.question = this.questions.pop();
+            const question = ` What is ${this.question.name}'s real name?`;
+            view.render(view.question, question);
+        } else {
+            this.gameOver();
         }
-        this.gameOver();
     },
-    ask() {
-        const question = ` What is ${this.question.name}'s real name?`;
-        view.render(view.question, question);
-        const response = prompt(question);
-        this.check(response);
-    },
-    check(response) {
+    check(event) {
+        event.preventDefault();
+        const response = view.response.answer.value;
         const answer = this.question.realName;
         if (response === answer) {
             view.render(view.result, 'Correct!', {'class': 'correct'});
-            alert('Correct!');
             this.score++;
             view.render(view.score, this.score);
         } else {
             view.render(view.result, `Wrong! The correct answer was ${answer}`, {'class': 'wrong'});
-            alert(`Wrong! The correct answer was ${answer}`);
         }
+        view.resetForm();
+        this.ask();
     },
     gameOver() {
         view.render(view.info, `Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
-        view.show(view.start);
-        alert(`Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.tearDown();
     }
 }
 
 view.start.addEventListener('click', () => game.start(quiz), false);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
 
 
