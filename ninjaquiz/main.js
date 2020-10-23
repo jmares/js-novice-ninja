374dacfbada3a9daa6fdc9db5fc3f72ac3348019
j0hm4r <67288966+j0hm4r@users.noreply.github.com>
2020-10-23 11:31:38 +0200

    quiz chapter 11

--- ninjaquiz/main.js
+++ ninjaquiz/main.js
@@ -1,3 +1,18 @@
+function random(a, b=1) {
+    // if only 1 value is provide we need to swap a and b
+    if (b === 1) {
+        [a,b] = [b,a];
+    }
+    return Math.floor((b-a+1) * Math.random()) + a;
+}
+
+function shuffle(array) {
+    for (let i = array.length; i; i--) {
+        let j = random(i) - 1;
+        [array[i-1], array[j]] = [array[j], array[i-1]];
+    }
+}
+
 const quiz = [
     { name: "Superman", realName: "Clark Kent"},
     { name: "Wonder woman", realName: "Diana Prince"},
@@ -59,6 +74,7 @@ const game = {
     ask(name) {
         console.log("ask() invoked");
         if (this.questions.length > 0) {
+            shuffle(this.questions);
             this.question = this.questions.pop();
             const question = ` What is ${this.question.name}'s real name?`;
             view.render(view.question, question);
