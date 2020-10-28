31b7796787c398692b9e067a7dae60d02013c144
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-10-28 13:16:56 +0100

    quiz chapter 15, doesn't work: deprecated packages, ...

---
+++ quiz/src/main.js
@@ -0,0 +1,11 @@
+import { view, game } from './quiz.js';
+
+const url = 'https://spbooks.github.io/jsninja2/questions.json';
+
+fetch(url)
+    .then(res => res.json())
+    .then(quiz => {
+        console.log('questions loaded!');
+        view.start.addEventListener('click', () => game.start(quiz.questions), false);
+        view.response.addEventListener('click', (event) => game.check(event), false);
+    });
